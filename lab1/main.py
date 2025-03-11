6e96661fcd000b38f0144c3247ff289897f1e0ea
sempaitakoo <idl76h@gmail.com>
2025-03-12 00:30:45 +0300

    Add calculate_task_3

--- lab1/main.py
+++ lab1/main.py
@@ -1,20 +1,28 @@
 from matrix.matrix import *
 
 
+def print_with_breaks(text: str, argument) -> None:
+    print(f'{text}:', argument, sep='\n', end='\n\n')
+
+
 def calculate_task_1(path_A: str, path_b: str) -> None:
     A = Matrix(from_filepath=path_A)
     b = Matrix(from_filepath=path_b)
 
+    print_with_breaks('A', A)
+    print_with_breaks('b', b)
+
     L, U = A.lu_decomposition()
-    print(f'L:', L, sep='\n', end='\n\n')
-    print(f'U:', U, sep='\n', end='\n\n')
+    print_with_breaks('L', L)
+    print_with_breaks('U', U)
 
     x = solve_lu(A, b)
-    print('x:', x, sep='\n', end='\n\n')
-    print('A * x = b:', check_solution(A, b, x), sep='\n', end='\n\n')
-    print('det(A):', A.determinant(), sep='\n', end='\n\n')
-    print('A^(-1):', A.inverse_matrix_lu(), sep='\n', end='\n\n')
-    print('A * A^(-1):', A * A.inverse_matrix_lu(), sep='\n', end='\n\n')
+    print_with_breaks('U', U)
+    print_with_breaks('x', x)
+    print_with_breaks('check_solution A * x = b', check_solution(A, b, x))
+    print_with_breaks('det(A)', A.determinant())
+    print_with_breaks('A^(-1)', A.inverse_matrix_lu())
+    print_with_breaks('A * A^(-1)', A * A.inverse_matrix_lu())
 
 
 def calculate_task_2(path_A: str, path_d: str) -> None:
@@ -22,11 +30,15 @@ def calculate_task_2(path_A: str, path_d: str) -> None:
     d = Matrix(from_filepath=path_d)
 
     a, b, c = get_three_diagonals(A)
-    print('diagonals:', *(a, b, c), sep='\n', end='\n\n')
+    print_with_breaks('diagonals', (a, b, c))
 
     x = thomas_algorithm(A, d)
-    print('thomas_algorithm:', x, sep='\n', end='\n\n')
-    print('check_solution:', check_solution(A, d, x), sep='\n', end='\n\n')
+    print_with_breaks('thomas_algorithm', x)
+    print_with_breaks('check solution A * x = d', check_solution(A, d, x))
+
+
+def calculate_task_3() -> None:
+    ...
 
 
 if __name__ == '__main__':
